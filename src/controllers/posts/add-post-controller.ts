ed92a871e575a338662d50890e4d132d2e2c302f
jeseias <domingosjeseias@gmail.com>
2021-12-22 15:56:46 +0100

    feat: ensure AddPostController returns 400 if requiredFields are not provided

--- src/controllers/posts/add-post-controller.ts
+++ src/controllers/posts/add-post-controller.ts
@@ -1,26 +1,32 @@
-import { badRequest, ok } from '../../helpers/http/http-helpers'
+import { MissingParamError } from '../../helpers/errors/missing-param-error'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helpers'
 import { HttpRequest, HttpResponse } from '../../helpers/http/http-protocols'
 import { AddPost } from '../../helpers/protocols/add-post'
-import { Validation } from '../../helpers/validators/validation-protocols'
 import { Controller } from '../controller-protocols'
 
 export class AddPostController implements Controller {
   constructor (
-    private readonly validation: Validation,
     private readonly addPost: AddPost
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const requiredFields = ['title', 'body', 'tags']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const { title, body, tags } = httpRequest.body
+
+      const post = await this.addPost.add({
+        title,
+        body,
+        tags
+      })
+      return ok(post)
+    } catch (error) {
+      return serverError(error)
     }
-    const { title, body, tags } = httpRequest.body
-    const post = await this.addPost.add({
-      title,
-      body,
-      tags
-    })
-    return ok(post)
   }
 }
--- src/helpers/http/http-helpers.ts
+++ src/helpers/http/http-helpers.ts
@@ -1,3 +1,4 @@
+import { ServerError } from '../errors'
 import { HttpResponse } from './http-protocols'
 
 export const badRequest = (error: Error): HttpResponse => ({
@@ -8,4 +9,9 @@ export const badRequest = (error: Error): HttpResponse => ({
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
