53c536cb82a18098454b8caa856aa244ac899d79
jeseias <domingosjeseias@gmail.com>
2022-01-09 14:51:09 +0100

    feat: ensure MongoHelper reconnects if connection is down

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   setupFilesAfterEnv: ['<rootDir>/.jest/jest-setup.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest'
--- src/helpers/mongo/mongo-helper.ts
+++ src/helpers/mongo/mongo-helper.ts
@@ -1,4 +1,5 @@
 import { PostModel } from '../../models/posts'
+import { MongoClient, Collection } from 'mongodb'
 
 export const map = (post: any): PostModel => {
   const mappedPost = Object.assign(
@@ -10,4 +11,25 @@ export const map = (post: any): PostModel => {
     })
 
   return mappedPost
+}
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect (uri: string): Promise<void> {
+    console.log(uri)
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {})
+  },
+
+  async disconnect (): Promise<void> {
+    if (this.client) await this.client.close()
+    this.client = null
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) await this.connect(this.uri)
+    return this.client.db().collection(name)
+  }
 }
