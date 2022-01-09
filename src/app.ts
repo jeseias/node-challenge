6730a14411a73037217701ed9b28e3b8e16ea69a
jeseias <domingosjeseias@gmail.com>
2022-01-09 18:40:21 +0100

    feat: add express secure middlewares

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,42 @@ import swaggerJsDocs from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerOptions } from './helpers/docs/swagger-options'
 import { postRoutes } from './routes/api/post'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
+import mongoSanitize from 'express-mongo-sanitize'
+import xXssProtection from 'x-xss-protection'
+import hpp from 'hpp'
 
 const app = express()
+
+app.enable('trust proxy')
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
+
+app.use(helmet())
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use('/api', limiter)
+
+app.use(mongoSanitize())
+
+app.use(xXssProtection())
+
+app.use(
+  hpp({
+    whitelist: [
+      'page',
+      'duration'
+    ]
+  })
+)
 
 const swaggerDocs = swaggerJsDocs(swaggerOptions)
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
