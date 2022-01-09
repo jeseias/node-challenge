a67b70ffdff9cc314cfd945e764fd7b03aeeec2e
jeseias <domingosjeseias@gmail.com>
2022-01-09 18:11:46 +0100

    feat: add mongo validations

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -14,10 +14,22 @@ export interface AddPostModel {
 }
 
 const PostSchema = new Schema({
-  _id: String,
-  title: String,
-  body: String,
-  tags: [String]
+  _id: {
+    type: String,
+    required: [true, 'Post id is required']
+  },
+  title: {
+    type: String,
+    required: [true, 'Post title is required']
+  },
+  body: {
+    type: String,
+    required: [true, 'Post body is required']
+  },
+  tags: {
+    type: [String],
+    required: [true, 'Post tags are required']
+  }
 })
 
 export const Post = model('Post', PostSchema)
